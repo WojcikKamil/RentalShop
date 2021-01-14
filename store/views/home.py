526849e7bca95ac1adfd57993e609e60eb6df38a
MitsuruKasahara <43825084+MitsuruKasahara@users.noreply.github.com>
2021-01-14 14:35:52 +0100

    Update

--- store/views/home.py
+++ store/views/home.py
@@ -3,15 +3,39 @@ from store.models.product import Product
 from store.models.category import Category
 from django.views import View
 
+
 # Create your views here.
 class Index(View):
     def post(self, request):
         product = request.POST.get('product')
-        print(product)
+        remove = request.POST.get('remove')
+        cart = request.session.get('cart')
+        if cart:
+            quantity = cart.get(product)
+            if quantity:
+                if remove:
+                    if quantity <= 1:
+                        cart.pop(product)
+                    else:
+                        cart[product] = quantity - 1
+                else:
+                    cart[product] = quantity + 1
+            else:
+                cart[product] = 1
+        else:
+            cart = {}
+            cart[product] = 1
+
+        request.session['cart'] = cart
+        print('cart', request.session['cart'])
         return redirect('homepage')
 
     def get(self, request):
+        cart = request.session.get('cart')
+        if not cart:
+            request.session['cart'] = {}
         products = None
+
         categories = Category.get_all_categories()
         categoryID = request.GET.get('category')
         if categoryID:
@@ -22,7 +46,5 @@ class Index(View):
         data = {}
         data['products'] = products
         data['categories'] = categories
-
         print(request.session.get('email'))
         return render(request, 'index.html', data)
-
